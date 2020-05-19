ec52da96c7d253179d9fdfd1519e20ce3d81f945
gtsop <gtsop+github@protonmail.com>
2020-05-19 19:48:35 +0100

    Replace multiple if statements with a for-loop

--- devbox.sh
+++ devbox.sh
@@ -1,25 +1,13 @@
 #!/usr/bin/env bash
 
-echo "Hello World!"
-
 source is-installed.bash
 
-isInstalled vim
+echo "Installed software:"
 
-if isInstalled vim; then
-  echo "[x] vim exists"
-else
-  echo "[ ] vim exists"
-fi
-
-if isInstalled tmux; then
-  echo "[x] tmux exists"
-else
-  echo "[ ] tmux exists"
-fi
-
-if isInstalled git; then
-  echo "[x] git exists"
-else
-  echo "[ ] git exists"
-fi
+for software in git tmux vim; do
+  if isInstalled $software; then
+    echo "[x] $software"
+  else
+    echo "[ ] $software"
+  fi
+done
